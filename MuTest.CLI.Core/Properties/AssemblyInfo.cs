b90f7f470955dedf06d5ee357a4c63e2ce7fbbfb
mdaniyalkhann <mdaniyal.khan@aurea.com>
2020-05-08 13:27:36 +0500

    updated versions

--- MuTest.CLI.Core/Properties/AssemblyInfo.cs
+++ MuTest.CLI.Core/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("5aa8f790-3eb7-4c66-9dd9-8894ccd00ff9")]
 
-[assembly: AssemblyVersion("3.1.0")]
-[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AssemblyVersion("3.2.0")]
+[assembly: AssemblyFileVersion("3.2.0")]
--- MuTest.Core/Properties/AssemblyInfo.cs
+++ MuTest.Core/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("a7a2e9a6-9927-41a9-b67d-ce76c5ca8f42")]
 
-[assembly: AssemblyVersion("4.1.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyVersion("4.2.0.0")]
+[assembly: AssemblyFileVersion("4.2.0.0")]
