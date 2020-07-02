23d17b270eec6def315e10343090b1f9f66372b9
mdaniyalkhann <mdaniyal.khan@aurea.com>
2020-07-02 18:05:17 +0500

    updated versions

--- MuTest.CLI.Core/Properties/AssemblyInfo.cs
+++ MuTest.CLI.Core/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("5aa8f790-3eb7-4c66-9dd9-8894ccd00ff9")]
 
-[assembly: AssemblyVersion("3.3.0")]
-[assembly: AssemblyFileVersion("3.3.0")]
+[assembly: AssemblyVersion("3.4.0")]
+[assembly: AssemblyFileVersion("3.4.0")]
