7ce71bc5206ab099505e0d383fbdc565106f481f
mdaniyalkhann <mdaniyal.khan@aurea.com>
2020-12-03 17:21:22 +0500

    fix cpp mutest code coverage issues

--- MuTest.Cpp.CodeGenerator/Common/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MuTest.Cpp.CodeGenerator.Common
-{
-    internal class Constants
-    {
-        internal const string EnterSolutionPath = "Enter Solution Path: ";
-        internal const string SolutionExtension = ".sln";
-        internal const string InvalidSolutionFileErrorMessage = "Solution File Not Exist Or Invalid File. Please Try Again...";
-    }
-}
